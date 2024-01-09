60b9bcc7f4943e163f1d8f9528bfe6d4c768bb4e
Shiqi Mei <shiqi.mei@askcodebase.com>
2024-01-09 18:08:13 +0800

    Add logo, title and description

--- src/app.py
+++ src/app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory('assets', 'logo.png')
 
 def dev():
     app.run(debug=True, port=8080)
