e422958348a1d41751da9bb4a072a6cf2694ec5f
Shiqi Mei <shiqi.mei@askcodebase.com>
2024-01-09 18:24:41 +0800

    Add authorize link for `Connect to GitHub`

--- src/app.py
+++ src/app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template, send_from_directory
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+        client_id=os.environ.get('GITHUB_OAUTH_CLIENT_ID'),
+        callback_url=os.environ.get('GITHUB_OAUTH_CALLBACK_URL')
+    )
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory('assets', 'logo.png')
 
 def dev():
-    app.run(debug=True, port=8080)
+    load_dotenv()
+    app.run(debug=True, host='0.0.0.0', port=8080)
 
 if __name__ == '__main__':
     dev()
