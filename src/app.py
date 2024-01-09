86ccd5cb77079e7a6431a7462ee47bc54ceec788
Shiqi Mei <shiqi.mei@askcodebase.com>
2024-01-09 16:25:09 +0800

    Add index.html

--- src/app.py
+++ src/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, redirect
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, Flask!"
+    return render_template('index.html')
 
 def dev():
     app.run(debug=True, port=8080)
